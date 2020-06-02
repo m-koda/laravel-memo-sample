f18e8f41728144b27a90aec2a93ba4dbd7de293d
m-koda <makotokoda82@gmail.com>
2020-06-02 19:24:31 +0900

    usersテーブルとmemosテーブルのhasMany(1対多)を定義

--- src/database/migrations/2020_06_02_090610_create_memos_table.php
+++ src/database/migrations/2020_06_02_090610_create_memos_table.php
@@ -17,6 +17,8 @@ class CreateMemosTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
